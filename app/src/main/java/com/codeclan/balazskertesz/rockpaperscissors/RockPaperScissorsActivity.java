37ff0eed831966e974b0552aca1a741d7e6524c8
Claire <cconnachan@gmail.com>
2018-04-25 11:10:23 +0100

    minor tweaks

--- app/src/main/java/com/codeclan/balazskertesz/rockpaperscissors/RockPaperScissorsActivity.java
+++ app/src/main/java/com/codeclan/balazskertesz/rockpaperscissors/RockPaperScissorsActivity.java
@@ -3,6 +3,7 @@ package com.codeclan.balazskertesz.rockpaperscissors;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -26,6 +27,7 @@ public class RockPaperScissorsActivity extends AppCompatActivity {
         paper = findViewById(R.id.buttonPaperID);
         instructions = findViewById(R.id.textDisplay);
         game = new Game();
+        Log.d(getClass().toString(), "In the Game onCreate");
 
 
         setButtonColors();
